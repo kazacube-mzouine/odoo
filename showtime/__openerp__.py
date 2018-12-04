0f4c8440bcc9d896ab1d2d6cf0e25c329b7092fb
Mohamed Zouine <mzouine@kazacube.com>
2018-12-04 14:46:25 +0100

    add two new modules showime for odoo test module upload

---
+++ showtime/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'showtime',
+    'version': '9.0.1.0.1',
+    'currency': 'EUR',
+    'price' : 49.99,
+    'category': 'Tools',
+    'author': "showtime",
+    'website': 'https://github.com/showtime',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+  
+    'installable': True,
+}
